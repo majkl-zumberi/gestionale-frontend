ebef391779bf3d4cde3880d4f880766680c203ed
Federico <federico.picone.develop@gmail.com>
2021-01-31 16:45:50 +0100

    added table and fixes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -30,7 +30,7 @@
           header
           class="text-grey-8"
         >
-          Essential Links
+          Dashboard
         </q-item-label>
         <EssentialLink
           v-for="link in essentialLinks"
@@ -51,46 +51,34 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksData = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
+    title: 'Articoli',
+    caption: 'Gestione Articoli',
+    icon: 'article',
     link: 'https://quasar.dev'
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
+    title: 'Clienti',
+    caption: 'Gestione Clienti',
+    icon: 'face',
     link: 'https://github.com/quasarframework'
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
+    title: 'Fatture',
+    caption: 'Gestione Fatture',
+    icon: 'receipt_long',
     link: 'https://chat.quasar.dev'
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
+    title: 'Ordini',
+    caption: 'Gestione Ordini',
+    icon: 'unarchive',
     link: 'https://forum.quasar.dev'
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
+    title: 'Measure Unit',
+    caption: 'Gestione Unità di Misura',
+    icon: 'straighten',
     link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
   }
 ];
 
---
+++ src/pages/Articles/ArticlesList.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-page padding>
+    <q-table
+      title="Articles"
+      :data="articles"
+      :columns="columns"
+      row-key="name"
+    />
+  </q-page>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+name: "articlesList",
+  components: {},
+  data(){
+  return {
+    columns:[
+      {name:"name", label:"Nome", type:"string", field: article => article.name},
+      {name:"category", label:"Categoria", type:"string"},
+      {name:"urlimg", label: "Immagine", type:"string"},
+      {name:"price", label:"Prezzo", type:"string"},
+      {name:"expiry", label:"Scadenza", type:"string"},
+      {name:"available", label:"Disponibilità", type:"string"},
+      {name:"measureId", label:"Unità di Misura", type:"string"},
+    ],
+    articles:[]
+  }
+  },
+  created: function(){
+    axios.get("http://localhost:3000/article").then(res => {
+     this.articles = res.data;
+    })
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
