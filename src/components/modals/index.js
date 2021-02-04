e0cb723aef53a800349d19af87ca83c1ce1fbe3f
majkl zumberi <majklzumberi00@gmail.com>
2021-02-04 21:05:49 +0100

    fix: :sparkles: register modals

--- src/components/modals/index.js
+++ src/components/modals/index.js
@@ -1,8 +1,12 @@
 import AddArticleModal from "./AddArticleModal";
+import CategoryModal from "./CategoryModal";
+import CustomerModal from "./CustomerModal";
 import DeleteModal from "./DeleteModal";
 import MeasureModal from "./MeasureModal";
 export default{
   AddArticleModal,
   DeleteModal,
-  MeasureModal
+  MeasureModal,
+  CategoryModal,
+  CustomerModal
 }
