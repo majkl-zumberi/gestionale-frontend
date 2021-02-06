f2bbd9b42f52581cd24704e263e9bd913061b8be
majkl zumberi <majklzumberi00@gmail.com>
2021-02-06 18:50:49 +0100

    refactor(component): :recycle: ordersList detail moved to detailListItem component

--- src/pages/Orders/OrdersList.vue
+++ src/pages/Orders/OrdersList.vue
@@ -97,93 +97,6 @@
         </q-table>
       </q-card-section>
     </q-card>
-    <q-dialog
-      v-model="dialog"
-      persistent
-      :maximized="maximizedToggle"
-      transition-show="slide-up"
-      transition-hide="slide-down"
-    >
-      <q-card class="bg-primary text-white">
-        <q-bar>
-          <q-space />
-
-          <q-btn dense flat icon="minimize" @click="maximizedToggle = false" :disable="!maximizedToggle">
-            <q-tooltip v-if="maximizedToggle" content-class="bg-white text-primary">Minimize</q-tooltip>
-          </q-btn>
-          <q-btn dense flat icon="crop_square" @click="maximizedToggle = true" :disable="maximizedToggle">
-            <q-tooltip v-if="!maximizedToggle" content-class="bg-white text-primary">Maximize</q-tooltip>
-          </q-btn>
-          <q-btn dense flat icon="close" v-close-popup>
-            <q-tooltip content-class="bg-white text-primary">Close</q-tooltip>
-          </q-btn>
-        </q-bar>
-
-        <q-card-section>
-          <div class="text-h6"></div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <!-- struttura tabell qua-->
-          <q-table
-            flat
-            title="Dettaglio Ordine"
-            :data="detailOrder.order"
-            :columns="detailColumns"
-            :filter="filter"
-            row-key="name"
-          >
-            <template v-slot:top-right>
-              <q-input
-                rounded
-                filled
-                dense
-                debounce="300"
-                v-model="filter"
-                placeholder="Cerca"
-              >
-                <template v-slot:append>
-                  <q-icon name="search" />
-                </template>
-              </q-input>
-            </template>
-            <template v-slot:body-cell-actions="props">
-              <q-td :props="props">
-                <q-btn
-                  size="sm"
-                  round
-                  dense
-                  color="primary"
-                  icon="edit"
-                  @click.stop="
-                  () => openNewArticleModal(props.row.id, 'AddArticle', 'edit')
-                "
-                  class="q-mr-xs"
-                />
-                <q-btn
-                  size="sm"
-                  round
-                  dense
-                  color="secondary"
-                  icon="delete"
-                  @click.stop="
-                  () =>
-                    openNewArticleModal(
-                      props.row.id,
-                      'article',
-                      'delete',
-                      props.row,
-                      props.row.name
-                    )
-                "
-                  class="q-mr-sm"
-                />
-              </q-td>
-            </template>
-          </q-table>
-        </q-card-section>
-      </q-card>
-    </q-dialog>
   </q-page>
 </template>
 
@@ -191,7 +104,8 @@
 import axios from "axios";
 import crudModal from "../../components/modals/CrudModal";
 import eventBus from "../../utils/eventBus";
-import { date } from "quasar";
+import { date, format } from "quasar";
+import DetailListItem from "./DetailListItem";
 export default {
   name: "ordersList",
   components: {},
@@ -202,86 +116,40 @@ export default {
           name: "id",
           label: "Nr Ordine",
           type: "string",
-          align: 'center',
+          align: "center",
           field: order => order.id
         },
         {
           name: "date",
           label: "Data ordine",
           type: "string",
-          align: 'center',
-          field: order => order.date
+          align: "center",
+          field: order => order.date,
+          format: data => date.formatDate(data, "DD-MM-YYYY")
         },
         {
           name: "ccode",
           label: "Codice Cliente",
           type: "string",
-          align: 'center',
-          field: order => order.user_id.ccode,
+          align: "center",
+          field: order => order.user_id.ccode
+        },
+        {
+          name: "businessName",
+          label: "Ragione Sociale",
+          type: "string",
+          align: "center",
+          field: order => order.user_id.businessName
         },
         {
           name: "actions",
           label: "Azioni",
-          align: 'center',
+          align: "center",
           type: "actions"
         }
       ],
       orders: [],
-      filter: "",
-      dialog: false,
-      maximizedToggle: true,
-      detailOrder: [],
-      detailColumns: [
-        {
-          name: "id",
-          label: "Codice Articolo",
-          type: "string",
-          align: 'center',
-          field: order => order.article.id
-        },
-        {
-          name: "name",
-          label: "Descrizione Articolo",
-          type: "string",
-          align: 'center',
-          field: order => order.article.name
-        },
-        {
-          name: "price",
-          label: "Prezzo",
-          type: "string",
-          align: 'center',
-          field: order => order.article.price
-        },
-        {
-          name: "createdAt",
-          label: "Data Creazione",
-          type: "string",
-          align: 'center',
-          field: order => order.article.createdAt
-        },
-        {
-          name: "iva",
-          label: "IVA",
-          type: "string",
-          align: 'center',
-          field: order => order.iva
-        },
-        {
-          name: "total",
-          label: "Imponibile",
-          type: "string",
-          align: 'center',
-          field: order => order.total
-        },
-        {
-          name: "totalIva",
-          label: "Totale",
-          type: "string",
-          align: 'center',
-          field: order => order.totalIva
-        },
-      ]
+      filter: ""
     };
   },
   methods: {
@@ -296,12 +164,18 @@ export default {
         parent: this
       });
     },
-
-    openDetailOrder: function (idOrder){
-      this.dialog = true
-      axios.get(`http://localhost:3000/detail-order/order/${idOrder}`).then(res => {
-        this.detailOrder = res.data;
-      });
+    openDetailOrder: function(idOrder) {
+      this.dialog = true;
+      axios
+        .get(`http://localhost:3000/detail-order/order/${idOrder}`)
+        .then(res => {
+          this.detailOrder = res.data;
+          this.$q.dialog({
+            component: DetailListItem,
+            detailOrder: res.data,
+            parent: this
+          });
+        });
     },
 
     updateList(newArticle) {
